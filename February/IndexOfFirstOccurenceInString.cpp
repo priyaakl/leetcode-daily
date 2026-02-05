2345a1f77f852c6a0734576aeef5d36cd9ccb37a
Priya <priyaakl44@gmail.com>
2026-02-05 23:31:32 +0530

    Feb 4

--- February/IndexOfFirstOccurenceInString.cpp
+++ February/IndexOfFirstOccurenceInString.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int strStr(string haystack, string needle) {
         if (needle == "") return 0;
+        if (needle.size() > haystack.size()) return -1;
 
         for (int i = 0; i <= haystack.size() - needle.size(); i++) {
             int j = 0;
